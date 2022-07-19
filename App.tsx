06f6202e93705297af7fdab1a024995af557b06a
lucasemanuelms <lucas.ufc1310@gmail.com>
2022-07-18 21:39:00 -0300

    Initial commit.

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { SignIn } from './src/screens/SignIn';
+import { THEME } from './src/styles/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent //Faz com que a barrinha superior meio que suma e deixe que o conteúdo ocupe a tela toda e não só dps da barrinha superior
+      />
+
+      {fontsLoaded ? <SignIn/> : <Loading/>}
+    </NativeBaseProvider>
+  );
+}
