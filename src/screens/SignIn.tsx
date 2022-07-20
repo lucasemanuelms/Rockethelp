406fcd256133d29e2266dee2faaeba4feb33d664
lucasemanuelms <lucas.ufc1310@gmail.com>
2022-07-19 21:06:10 -0300

    style: Done Sign In layout.

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,45 @@
-import {Heading, VStack} from 'native-base'
+import { useState } from 'react'
+import {Heading, VStack, Icon, useTheme} from 'native-base'
+import Logo from '../assets/logo_primary.svg'
+import { Input } from '../components/Input'
+import { Envelope, Key } from 'phosphor-react-native'
+import { Button } from '../components/Button' 
 
 export function SignIn(){
+
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+
+    function handleSignIn(){
+        console.log(name, password)
+    }
+
+    const {colors} = useTheme()
+
     return(
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}> 
+            <Logo/>
             <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
                 Acesse sua conta
             </Heading>
+
+            <Input 
+                mb={4}
+                placeholder="E-mail"
+                InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4}/>}
+                onChangeText={setName}
+            />
+
+            <Input 
+                mb={8}
+                placeholder="Senha" 
+                InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4}/>}
+                secureTextEntry
+                onChangeText={setPassword}
+            />
+
+            <Button title='Entrar' w="full" onPress={handleSignIn}/>
+
         </VStack>
     )
 }
