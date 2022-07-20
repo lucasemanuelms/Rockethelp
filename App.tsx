3f0d74bd6fa57152a607dd8a4ec2b476813af685
lucasemanuelms <lucas.ufc1310@gmail.com>
2022-07-19 22:54:25 -0300

    feat: Made the functionality to switch to progress or finished.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
-import { SignIn } from './src/screens/SignIn';
-import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
+
+import { Home } from './src/screens/Home';
 import { Loading } from './src/components/Loading';
+
 
 export default function App() {
 
@@ -16,7 +19,7 @@ export default function App() {
         translucent //Faz com que a barrinha superior meio que suma e deixe que o conteúdo ocupe a tela toda e não só dps da barrinha superior
       />
 
-      {fontsLoaded ? <SignIn/> : <Loading/>}
+      {fontsLoaded ? <Home/> : <Loading/>}
     </NativeBaseProvider>
   );
 }
